bcdc13496da1ed98af53a57e5f19b7a4a42ab8fb
ericmoore0709 <159437312+ericmoore0709@users.noreply.github.com>
2025-12-04 15:55:11 +0000

    refactored hardcoded credentials to .env file

--- dm-notes/lib/db.ts
+++ dm-notes/lib/db.ts
@@ -1,7 +1,10 @@
 import { Client } from 'pg';
 
 const db = new Client({
-    connectionString: process.env.DATABASE_URL
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST
 });
 
 await db.connect();
