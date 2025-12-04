a3adcd7f66c5a585e0babb1242907d9a70b2256a
ericmoore0709 <159437312+ericmoore0709@users.noreply.github.com>
2025-12-04 22:15:01 +0000

    added characters POST endpoint

--- dm-notes/app/api/v1/stat-blocks/characters/route.ts
+++ dm-notes/app/api/v1/stat-blocks/characters/route.ts
@@ -4,3 +4,19 @@ export async function GET(request: Request) {
     const result = await db.query('SELECT * FROM characters');
     return Response.json({ data: result.rows, error: false });
 }
+
+export async function POST(request: Request) {
+    let data;
+
+    try {
+        data = await request.json() || null;
+    } catch (e400) {
+        console.log(e400);
+        return Response.json({message: "Request body required.", error: true}, {status: 400});
+    }
+    const {name} = data;
+    if (!name) return Response.json({message: "'name' is required.", error: true}, {status: 400});
+
+    const result = await db.query('INSERT INTO characters (name) VALUES ($1) RETURN id, name', [name]);
+    return Response.json({data: result.rows[0], error: false});
+}
