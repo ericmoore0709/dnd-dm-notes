039d03570727c97f723e048c9622648d17749457
ericmoore0709 <159437312+ericmoore0709@users.noreply.github.com>
2025-12-08 23:05:21 +0000

    updated CharacterForm to reflect current Character data

--- dm-notes/app/stat-blocks/characters/CharacterForm.tsx
+++ dm-notes/app/stat-blocks/characters/CharacterForm.tsx
@@ -3,7 +3,7 @@ import { ChangeEvent, FormEvent, useState } from "react";
 import { Character } from "./Character";
 
 export default function CharacterForm({ formAction, existingData }: { formAction: Function, existingData: Character | null }) {
-    const INITIAL_FORM_DATA = { id: '' };
+    const INITIAL_FORM_DATA = { name: '' };
     const [formData, setFormData] = useState(existingData || INITIAL_FORM_DATA);
 
     function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
@@ -20,8 +20,8 @@ export default function CharacterForm({ formAction, existingData }: { formAction
     return (
         <form className="w-75 mx-auto my-2 py-2" onSubmit={handleFormSubmit}>
             <div className="flex justify-center">
-                <label className="mr-2" htmlFor="id">ID</label>
-                <input className="border rounded cursor-text" id="id" name="id" value={formData.id} onChange={handleInputChange} />
+                <label className="mr-2" htmlFor="name">Name</label>
+                <input className="border rounded cursor-text" id="name" name="name" value={formData.name} onChange={handleInputChange} />
             </div>
             <button className="mt-2 border rounded p-1 cursor-pointer" type="submit">Go!</button>
         </form>
