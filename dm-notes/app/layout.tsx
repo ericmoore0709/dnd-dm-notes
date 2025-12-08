4fbee0f6633a64cab12beb05703b066cec3a4706
ericmoore0709 <159437312+ericmoore0709@users.noreply.github.com>
2025-12-08 23:47:00 +0000

    added navbar to site layout

--- dm-notes/app/layout.tsx
+++ dm-notes/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="pt-2 pb-1 bg-red-500 border-b">
+          <nav className="flex items-baseline">
+            <div className="text-xl mx-2"><Link href={"/"}>DM Notes</Link></div>
+            <ul className="mx-2 my-0 text-sm">
+              <li className="mr-2 hover:underline"><Link href={"/stat-blocks/characters"}>Characters</Link></li>
+            </ul>
+          </nav>
+        </div>
         {children}
       </body>
     </html>
