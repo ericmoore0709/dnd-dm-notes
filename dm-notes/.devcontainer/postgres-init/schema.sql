cd9102f83d997d7c3d6755dfa717917cb7e2c9b9
ericmoore0709 <159437312+ericmoore0709@users.noreply.github.com>
2025-12-09 17:42:38 +0000

    added weapons and ability scores to database

--- dm-notes/.devcontainer/postgres-init/schema.sql
+++ dm-notes/.devcontainer/postgres-init/schema.sql
@@ -1,11 +1,46 @@
-DROP TABLE IF EXISTS characters, abilities;
+DROP TABLE IF EXISTS characters,
+abilities,
+ability_scores,
+weapons,
+monsters,
+monster_weapons,
+monster_ability_scores;
 
 CREATE TABLE IF NOT EXISTS characters (
-    id SERIAL,
+    id SMALLSERIAL PRIMARY KEY UNIQUE,
     name VARCHAR(50) UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS abilities (
-    id SERIAL,
+    id SMALLSERIAL PRIMARY KEY UNIQUE,
     name VARCHAR(20) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ability_scores (
+    id SMALLSERIAL PRIMARY KEY UNIQUE,
+    ability_id SMALLINT REFERENCES abilities (id),
+    score SMALLINT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS weapons (
+    id SMALLSERIAL PRIMARY KEY UNIQUE,
+    name VARCHAR(50) UNIQUE NOT NULL,
+    damage_dice VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS monsters (
+    id SMALLSERIAL PRIMARY KEY UNIQUE,
+    name VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS monster_weapons (
+    id SMALLSERIAL PRIMARY KEY UNIQUE,
+    monster_id SMALLINT REFERENCES monsters (id),
+    weapon_id SMALLINT REFERENCES weapons (id)
+);
+
+CREATE TABLE IF NOT EXISTS monster_ability_scores (
+    id SMALLSERIAL PRIMARY KEY UNIQUE,
+    monster_id SMALLINT REFERENCES monsters (id),
+    ability_score SMALLINT REFERENCES ability_scores (id)
 );
--- dm-notes/.devcontainer/postgres-init/seed.sql
+++ dm-notes/.devcontainer/postgres-init/seed.sql
@@ -1,3 +1,45 @@
-INSERT INTO characters (name) VALUES ('Gundren Rockseeker'), ('Sildar Hallwinter');
+INSERT INTO
+    characters (name)
+VALUES ('Gundren Rockseeker'),
+    ('Sildar Hallwinter');
 
-INSERT INTO abilities (name) VALUES ('STR'), ('DEX'), ('CON'), ('INT'), ('WIS'), ('CHA');
+INSERT INTO
+    abilities (name)
+VALUES ('STR'),
+    ('DEX'),
+    ('CON'),
+    ('INT'),
+    ('WIS'),
+    ('CHA');
+
+INSERT INTO
+    weapons (name, damage_dice)
+VALUES ('Scimitar', '1d6'),
+    ('Longsword', '1d8'),
+    ('Greatsword', '2d8');
+
+INSERT INTO
+    monsters (name)
+VALUES ('Goblin'),
+    ('Hobgoblin'),
+    ('Bugbear');
+
+INSERT INTO monster_weapons (monster_id, weapon_id) VALUES (1, 1);
+
+INSERT INTO
+    ability_scores (ability_id, score)
+VALUES (1, 8),
+    (2, 14),
+    (3, 10),
+    (4, 10),
+    (5, 8),
+    (6, 8);
+
+INSERT INTO
+    monster_ability_scores (monster_id, ability_score)
+VALUES (1, 1),
+    (1, 2),
+    (1, 3),
+    (1, 4),
+    (1, 5),
+    (1, 6);
