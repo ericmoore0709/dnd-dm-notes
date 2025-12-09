c2f06fc1538719d15873885c545944089a28b435
ericmoore0709 <159437312+ericmoore0709@users.noreply.github.com>
2025-12-09 13:50:23 +0000

    added abilities table to database

--- dm-notes/.devcontainer/postgres-init/schema.sql
+++ dm-notes/.devcontainer/postgres-init/schema.sql
@@ -1,6 +1,11 @@
-DROP TABLE IF EXISTS characters;
+DROP TABLE IF EXISTS characters, abilities;
 
 CREATE TABLE IF NOT EXISTS characters (
     id SERIAL,
     name VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS abilities (
+    id SERIAL,
+    name VARCHAR(20) UNIQUE NOT NULL
 );
--- dm-notes/.devcontainer/postgres-init/seed.sql
+++ dm-notes/.devcontainer/postgres-init/seed.sql
@@ -1,1 +1,3 @@
 INSERT INTO characters (name) VALUES ('Gundren Rockseeker'), ('Sildar Hallwinter');
+
+INSERT INTO abilities (name) VALUES ('STR'), ('DEX'), ('CON'), ('INT'), ('WIS'), ('CHA');
